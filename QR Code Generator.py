96f3d0a4d93386ab9e3c8bc82e061726a1c8c0da
Nikhil Gupta <43096630+nikkkhil067@users.noreply.github.com>
2020-09-04 13:04:39 +0530

    Create QR Code Generator.py
    Requirements :
    1. pip install pyqrcode
    2. pip install pypng

---
+++ QR Code Generator.py
@@ -0,0 +1,11 @@
+import pyqrcode
+from pyqrcode import QRCode
+import png
+
+str = "String which represent the QR Code message or the reference link"
+
+gen = pyqrcode.create(str)  #Generating QR Code
+
+gen.svg("myqrcode.svg", scale = 8 ) #Saving the qrcode generated in svg file
+
+gen.png("myqrcode.png", scale = 8) #Saving the qrcode generated in png file.
