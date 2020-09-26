65a98b2269a76f0dd85589eca1b2a76a62b48cf6
Nikhil Gupta <43096630+nikkkhil067@users.noreply.github.com>
2020-09-26 14:31:19 +0530

    Update Cartooning Images.py

--- Cartooning Images.py
+++ Cartooning Images.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("picture_name.jpg")
+img = cv2.imread(r"picture_name.jpg")
 
 #Getting Edges
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.medianBlur(grey, 5)
+gray = cv2.medianBlur(gray, 5)
 edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
 
 #Cartoonization
